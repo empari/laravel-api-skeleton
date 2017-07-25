4eb6baba595f3547de991372764805fe2cb09d97
Adriano Santos <adrianodrix@gmail.com>
2017-07-25 18:37:57 -0300

    APISKEL-3 Remover todos os arquivos referente a View Web #time 10m #done

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Skel'),
 
     /*
     |--------------------------------------------------------------------------
@@ -225,7 +225,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
     ],
 
 ];
